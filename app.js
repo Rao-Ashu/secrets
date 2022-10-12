4a2ee750655ed2115de99271e662572218fce620
Rao-Ashu <mit2021066@iiita.ac.in>
2022-10-12 11:20:25 +0530

    level 1 authentication, using register and login

---
+++ app.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+
+
+
+// HOME_Page (using express chain routing)
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+
+
+// REGISTER_Page (using express chain routing)
+app.route("/register")
+
+.get(function(req,res){
+  res.render("register");
+})
+
+.post(function(req,res){
+  const user1 = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  user1.save(function(err){
+    if(err){
+      res.send(err);
+    }else{
+      res.redirect("/login")
+    }
+  });
+
+});
+
+
+
+// LOGIN_Page (using express chain routing)
+app.route("/login")
+
+.get(function(req,res){
+  res.render("login");
+})
+
+.post(function(req,res){
+  User.findOne({email: req.body.username},function(err,result){
+    if(err){
+      console.log(err);
+    }else if(result){
+      if(result.password === req.body.password){
+        res.render("secrets");
+      }
+    }
+  })
+});
+
+
+
+app.listen(3000);
