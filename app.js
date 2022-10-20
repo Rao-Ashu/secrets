85d536fcab150d8b01b382f70d3bd65835660848
Rao-Ashu <mit2021066@iiita.ac.in>
2022-10-21 01:15:10 +0530

    add login with google, using OAuth2.0 Level 6

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const mongoose = require("mongoose");
 // const saltRounds = 10;
 
 const session = require("express-session");
-const passport = require("passport");                                         //STEP 1
+const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;                  //STEP 1
+const findOrCreate = require("mongoose-findorcreate");                               //STEP 3
 
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 
 app.use(session({
-  secret:process.env.SECRET,                                                 //STEP 2
+  secret:process.env.SECRET,
   resave:false,
   saveUninitialized: false
 }));
@@ -35,20 +35,43 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,                                                                //STEP 8 to add new fields googleId and secret ehich they will post
+  secret: String
 });
 
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-userSchema.plugin(passportLocalMongoose);                                      //STEP 3
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);                                                      //STEP 4
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-                                                                              //STEP 4
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, cb) {                                         //STEP 7
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id,function(id, user){
+    cb(err,user);
+  });
+});
 
 
+
+passport.use(new GoogleStrategy({                                                       //STEP 2
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 // HOME_Page (using express chain routing)
@@ -56,6 +79,18 @@ app.get("/", function(req,res){
   res.render("home");
 });
 
+
+// TO GET THE GOOGLE LOGIN PAGE AFTER CLICKING LOGIN WITH GOOGLE
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ['profile'] })                          //STEP 5
+)
+
+app.get('/auth/google/secrets',                                                   //STEP 6
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
+  });
 
 
 
